a0c4abef9beb8ef82caa3fac92f5953a4cfcf9e5
Peter Soule <Peter.Soule@viadex.com>
2024-03-03 11:10:14 +0200

    Added code omitted by Copilot

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -43,6 +43,14 @@ $localipaddress = @(Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration
 
 # Register argument completer for winget command
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
 start-transcript -append -outputdirectory "C:\Users\psoule\OneDrive - VIADEX\Documents\WindowsPowerShell\PSLogs"
 
 Set-Alias pcx C:\sysTools\procexp.exe
